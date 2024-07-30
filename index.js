324d1617ceffdbd1d1c8e2f258ce5992d20f17bc
Erik Slovak <erik.slovak@technologystudio.sk>
2024-07-30 11:42:02 +0200

    fix: refactor to use commit hash

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 
 const accessToken = process.env.GITHUB_ACCESS_TOKEN;
-const commitRef = process.env.COMMIT_REF;
+const commitSha = process.env.COMMIT_SHA;
 const repo = process.env.REPO_SLUG;
 const owner = process.env.REPO_OWNER;
 const bitriseStatus = process.env.BITRISE_BUILD_STATUS;
@@ -14,54 +14,43 @@ const isBuildFailed = process.env.IS_BUILD_FAILED;
   if (isBuildFailed === undefined && bitriseStatus === '1') {
     execSync(`envman add --key IS_BUILD_FAILED --value true`);
   }
+  if (commitSha === "") {
+    console.log("No commit SHA provided, skipping");
+    return;
+  }
 
-  console.log(`Fetching commit info for ${commitRef} on ${owner}/${repo}`);
-  console.log(`URL: https://api.github.com/repos/${owner}/${repo}/commits/${commitRef}`);
-  const getCommitResponse = await fetch(`https://api.github.com/repos/${owner}/${repo}/commits/${commitRef}`, {
-    method: 'GET',
+  const state = bitriseStatus === "0"
+      ? isPending === "true"
+        ? "pending"
+        : "success"
+      : "failure";
+  const description = state === "success"
+    ? 'The build succeeded'
+    : state === "failure"
+      ? 'The build failed'
+      : state === "pending"
+        ? 'The build is pending'
+        : 'The build failed';
+      
+  console.log(`Updating status for ${commitSha} to ${state} with description ${description}`);
+  const newStatusResponse = await fetch(`https://api.github.com/repos/${owner}/${repo}/statuses/${commitSha}`, {
+    method: 'POST',
     headers: {
       'Authorization': `Bearer ${accessToken}`,
-      'Accept': 'application/vnd.github.sha',
+      'Accept': 'application/vnd.github.v3+json',
       'X-GitHub-Api-Version': '2022-11-28',
     },
+    body: JSON.stringify({
+      target_url: buildUrl,
+      context: `ci/bitrise/${buildNumber}`,
+      state,
+      description,
+    }),
   });
-  if (getCommitResponse.ok) {
-    const commitSha = await getCommitResponse.text();
-    const state = bitriseStatus === "0"
-        ? isPending === "true"
-          ? "pending"
-          : "success"
-        : "failure";
-    const description = state === "success"
-      ? 'The build succeeded'
-      : state === "failure"
-        ? 'The build failed'
-        : state === "pending"
-          ? 'The build is pending'
-          : 'The build failed';
-        
-    console.log(`Updating status for ${commitSha} to ${state} with description ${description}`);
-    const newStatusResponse = await fetch(`https://api.github.com/repos/${owner}/${repo}/statuses/${commitSha}`, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-        'Accept': 'application/vnd.github.v3+json',
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-      body: JSON.stringify({
-        target_url: buildUrl,
-        context: `ci/bitrise/${buildNumber}`,
-        state,
-        description,
-      }),
-    });
 
-    if (newStatusResponse.ok) {
-      console.log("Successfully updated status");
-    } else {
-      throw new Error("Failed to update status");
-    }
+  if (newStatusResponse.ok) {
+    console.log("Successfully updated status");
   } else {
-    throw new Error("Failed to get statuses and commit: " + getCommitResponse.statusText);
+    throw new Error("Failed to update status");
   }
 })();
